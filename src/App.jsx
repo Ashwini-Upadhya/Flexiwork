73ec13fb87e93cf62a354471567a55b0e4ca17b2
Delta Student <113283944+Akshat-Rodwal@users.noreply.github.com>
2024-09-17 14:38:37 +0530

    akshat

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Flexiwork from './Flexiwork'
+import Hero_Section from './Hero_Section'
+import Navbar from './Navbar'
 import Posts from './Post'
 function App() {
   return <div>
-    <Flexiwork/>
-    <Posts/>
+    {/* <Flexiwork/>
+    <Posts/> */}
+    <Navbar/>
+    <Hero_Section/>
     </div>
 }
 export default App
